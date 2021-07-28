39d0c7abb822ec20cfde1fc7a8b53281791d7b9e
DouglasRamirez1 <douglasram4018@gmail.com>
2021-07-27 22:03:40 -0500

    renaming stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,19 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Car car = new Car();
-        car.setColor("white");
-        car.setSpeed(120);
+        Car c = new Car();
+        c.setColor("white");
+        c.setSpeed(120);
 
         Motorcycle m = new Motorcycle();
         m.setColor("black");
         m.setSpeed(160);
 
         System.out.println(
-            "I have a car! It is the color " + car.getColor()
-                    + ", the max speed is " + car.getSpeed()
-                    + ", the number of wheels it has is " + car.getNumOfWheels()
-                    + ", and it having a windshield is " + car.hasWindshield());
+            "I have a car! It is the color " + c.getColor()
+                    + ", the max speed is " + c.getSpeed()
+                    + ", the number of wheels it has is " + c.getNumOfWheels()
+                    + ", and it having a windshield is " + c.hasWindshield());
 
         System.out.println(
                 "I have a motorcycle! It is the color " + m.getColor()
